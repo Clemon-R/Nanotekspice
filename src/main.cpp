54cee37c0d4055ccad6a5a3433ed9320a9ef442b
Raphaël <raphael.goulmot@epitech.eu>
2018-02-07 16:39:29 +0100

    error 84

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,22 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		throw Exception("Too few arguments");
-	Parser::parseFile(std::string(argv[1]));
-	Parameter::parseParams(2, argc, argv);
-	Parser::checkSettings();
-	for (const auto &elem : Database::getComponents()){
-		if (std::get<0>(elem.second) == Database::Type::OUTPUT){
-			elem.first->compute(1);
-			elem.first->dump();
+	try{
+		if (argc < 2)
+			throw Exception("Too few arguments");
+		Parser::parseFile(std::string(argv[1]));
+		Parameter::parseParams(2, argc, argv);
+		Parser::checkSettings();
+		for (const auto &elem : Database::getComponents()){
+			if (std::get<0>(elem.second) == Database::Type::OUTPUT){
+				elem.first->compute(1);
+				elem.first->dump();
+			}
 		}
+		std::make_unique<Command>();
 	}
-	std::make_unique<Command>();
+	catch (std::exception &error){
+		return (84);
+	}
 	return (0);
 }
